f5a1ffe19aeb8ffb7998f558e73bf8200c996e9d
thomasmoreaumaster <thomas.moreau.master@gmail.com>
2017-02-01 03:14:06 +0100

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -16,8 +16,7 @@ package main
 import (
 	"errors"
 	"fmt"
-
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/tree/master/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
